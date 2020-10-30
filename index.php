cb7a18fa6e224671f1ee72c7fca80c570e7efc92
kanakarn123 <73680257+kanakarn123@users.noreply.github.com>
2020-10-30 10:55:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php
-echo "<h1>คณากานต์ ศรีสมบูรณ์สุข</h1>";
-echo "<h2>จ๊ะโอ๋</h2>";
-<img src="cat.jpg">
-<img src="dog.jpeg">
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <p>คณากานต์ ศรีสมบูรณ์สุข จ๊ะโอ๋</p>
+        <img src="cat.jpg">
+        <img src="dog.jpeg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
